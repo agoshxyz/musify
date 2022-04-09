17dfbdb5058b7db0cfb048a17b2be2f074a83c2d
agosh <agageldidurdyyev@gmail.com>
2022-04-09 21:04:35 +0300

    sidebar icons

---
+++ components/sidebar.tsx
@@ -0,0 +1,15 @@
+import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+
+
+} from 'react-icons/md';
+const Sidebar = () => {
+  return <div>sidebar</div>;
+};
+
+export default Sidebar;
