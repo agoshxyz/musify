9458a417a90542330c8b99ca8cfefd8a9e2a4c91
agosh <agageldidurdyyev@gmail.com>
2022-04-09 23:43:07 +0300

    my nice logo

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -3,11 +3,11 @@ import Sidebar from "../components/sidebar";
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
-        <Box position="absolute" top="0" width="250px" left="0">
+        <Box position="absolute" top="0" width="240px" left="0">
             <Sidebar/>
         </Box>
 
-        <Box marginLeft="250px" marginBottom="100px">
+        <Box marginLeft="240px" marginBottom="100px">
             {children}
         </Box>
 
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,15 +1,38 @@
-import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import NextImage from "next/image";
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
 import {
   MdHome,
   MdSearch,
   MdLibraryMusic,
   MdPlaylistAdd,
   MdFavorite,
-
-
-} from 'react-icons/md';
+} from "react-icons/md";
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="200px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" width={200} height={100}/>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Sidebar;
